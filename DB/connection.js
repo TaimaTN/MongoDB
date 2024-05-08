79c82d40619f4bde7d539e4571ff29f2fd18f003
TaimaTN <taima598awwad@gmail.com>
2024-05-09 00:51:13 +0300

    set the enveroment

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB =async()=>{
   
-      return  mongoose.connect(`mongodb+srv://taima123:taima1234@cluster0.ce0csjy.mongodb.net/mongo`)
+      return  mongoose.connect(`${process.env.DB}`)
       .then(()=>{
         console.log("sucess to connect DB");
       }).catch(er=>{
--- src/app.route.js
+++ src/app.route.js
@@ -4,9 +4,9 @@ import userRouter from "./moduels/user/router.js";
 const initApp=(app,express)=>{
     connectDB();
     app.use(express.json());
-    // app.use('/',(req,res)=>{
-    //     return res.json({wELLCOME:"HOME PAGE"});
-    // });
+    app.get('/',(req,res)=>{
+        return res.json({wELLCOME:"HOME PAGE"});
+    });
     app.use('/auth',authRouter);
     app.use('/user',userRouter);
     app.use('/*',(req,res)=>{
